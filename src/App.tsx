649864310366dcad65d9e15aec33dfac37c6a3ea
Mark AC <m4rkd0tc0de@gmail.com>
2023-05-11 07:07:47 +0800

    add hangman drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { HangmanDrawing } from "./HangmanDrawing copy 2"
 
 
 const App = () => {
   return (
-    <div>
-      <h1>Hi</h1>
+    <div className="max-w-4xl flex flex-col gap-8 my-0 mx-auto items-center">
+      <div className="text-4xl text-center">
+        Winner Loser
+      </div>
+      <HangmanDrawing />
     </div>
   )
 }
---
+++ src/HangmanDrawing copy 2.tsx
@@ -0,0 +1,11 @@
+
+export function HangmanDrawing() {
+  return (
+    <div className="relative">
+      <div className="h-14 w-2.5 bg-black top-0 right-0 absolute" />
+      <div className="h-2.5 w-52 bg-black ml-32" />
+      <div className="h-96 w-2.5 bg-black ml-32"/>
+      <div className="h-2.5 w-64 bg-black" />
+    </div>
+  )
+}
