855012eae82dbd8da95b2f8576ec73ba967ef9ad
Mark AC <m4rkd0tc0de@gmail.com>
2023-05-11 20:42:58 +0800

    modify hangman word component

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,13 +1,25 @@
-const word = "Utilities"
+type HangmanProps = {
+  guessedLetters: string[]
+  wordToGuess: string
+  reveal?: boolean
+}
 
 
 
-export function HangmanWord() {
+export function HangmanWord({
+  guessedLetters,
+  wordToGuess,
+  reveal = false,
+}: HangmanProps) {
+
+
   return (
     <div className="flex gap-1 text-8xl font-bold uppercase font-mono">
-      {word.split("").map((letter, index) =>
-        <span className="border-b-[0.1em] border-black">
-          <span>
+      {wordToGuess.split("").map((letter, index) =>
+        <span className="border-b-[0.1em] border-black" key={index}>
+          <span className={`${guessedLetters.includes(letter) || reveal ? "visible" : "invisible"}    ${!guessedLetters.includes(letter) && reveal ? "text-red-500": "text-black"}`} 
+          
+        >
             {letter}
           </span>
         </span>
